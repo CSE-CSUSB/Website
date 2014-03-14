ae8a510054d4213716e7660ec49c2c21c38461ba
Mike Korcha <mikekorcha@gmail.com>
2014-03-14 15:57:30 -0700

    Adding LAN party reservation form

---
+++ public/lanres.php
@@ -0,0 +1,17 @@
+<?php
+	// Data file
+	$res_file = "../lanres.txt";
+		
+	// Input check
+	if(empty($_POST['cid']) ||
+	   empty($_POST['name'])) {
+		header("Refresh:3; lanres.html", true, 303);
+		die("Please enter your name and Coyote ID. You will be sent back to the form.");
+	}
+
+	// Add reservation to file
+	file_put_contents($res_file, $_POST['cid'] . " - " . $_POST['name'] . "\n", FILE_APPEND | LOCK_EX);
+		    
+	echo "Your reservation has been made!";
+
+	header("Refresh:3; url=http://cse-club.com", true, 303);
