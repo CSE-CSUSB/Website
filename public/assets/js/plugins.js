8b30cd8884497da5e1e365072cd65f5c86e02e70
Andrew Brinker <me@andrewbrinker.com>
2014-04-08 17:12:26 -0700

    Added nicer nav scrolling

--- public/assets/js/plugins.js
+++ public/assets/js/plugins.js
@@ -1,2 +1,37 @@
 // Avoid `console` errors in browsers that lack a console.
 (function(){var e,t=function(){},n=["assert","clear","count","debug","dir","dirxml","error","exception","group","groupCollapsed","groupEnd","info","log","markTimeline","profile","profileEnd","table","time","timeEnd","timeStamp","trace","warn"],r=n.length,i=window.console=window.console||{};while(r--)e=n[r],i[e]||(i[e]=t)})();
+
+/*!
+ * jquery.scrollto.js 0.0.1 - https://github.com/yckart/jquery.scrollto.js
+ * Scroll smooth to any element in your DOM.
+ *
+ * Copyright (c) 2012 Yannick Albert (http://yckart.com)
+ * Licensed under the MIT license (http://www.opensource.org/licenses/mit-license.php).
+ * 2013/02/17
+ **/
+$.scrollTo = $.fn.scrollTo = function (x, y, options) {
+  if (!(this instanceof $)) {
+    return $.fn.scrollTo.apply($('html, body'), arguments);
+  }
+
+  options = $.extend({}, {
+    gap: {
+      x: 0,
+      y: 0
+    },
+    animation: {
+      easing: 'swing',
+      duration: 600,
+      complete: $.noop,
+      step: $.noop
+    }
+  }, options);
+
+  return this.each(function () {
+    var elem = $(this);
+    elem.stop().animate({
+      scrollLeft: !isNaN(Number(x)) ? x : $(y).offset().left + options.gap.x,
+      scrollTop: !isNaN(Number(y)) ? y : $(y).offset().top + options.gap.y
+    }, options.animation);
+  });
+};
