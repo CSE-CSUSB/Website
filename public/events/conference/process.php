e876a95651de75697075c666413f8da9867d6092
Mike Korcha <mikekorcha@gmail.com>
2014-08-28 23:39:35 -0700

    Adding conference RSVP page

---
+++ public/events/conference/process.php
@@ -0,0 +1,17 @@
+<?php
+	// Data file
+	$res_file = "../../conference_rsvp.txt";
+		
+	// Input check
+	if(empty($_POST['cid']) ||
+	   empty($_POST['name'])) {
+		header("Refresh:3; /events/conference", true, 303);
+		die("Please enter your name and Coyote ID. You will be sent back to the form.");
+	}
+
+	// Add reservation to file
+	file_put_contents($res_file, $_POST['cid'] . " - " . $_POST['name'] . " - " . $_POST['concerns'] . "\n", FILE_APPEND | LOCK_EX);
+		    
+	echo "You have successfully RSVP'd! We look forward to seeing you.";
+
+	header("Refresh:3; url=http://cse-club.com", true, 303);
