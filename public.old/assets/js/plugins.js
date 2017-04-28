9641611778e0e424aba6a22281b1adc9c198dd7b
chid9202 <chid9202@gmail.com>
2017-04-28 23:09:15 +0000

    upgrade

---
+++ public.old/assets/js/plugins.js
@@ -0,0 +1,40 @@
+// Avoid `console` errors in browsers that lack a console.
+(function(){var e,t=function(){},n=["assert","clear","count","debug","dir","dirxml","error","exception","group","groupCollapsed","groupEnd","info","log","markTimeline","profile","profileEnd","table","time","timeEnd","timeStamp","trace","warn"],r=n.length,i=window.console=window.console||{};while(r--)e=n[r],i[e]||(i[e]=t)})();
+
+/*!
+ * jquery.scrollto.js 0.0.1 - https://github.com/yckart/jquery.scrollto.js
+ * Scroll smooth to any element in your DOM.
+ *
+ * Copyright (c) 2012 Yannick Albert (http://yckart.com)
+ * Licensed under the MIT license (http://www.opensource.org/licenses/mit-license.php).
+ * 2013/02/17
+ **/
+(function ($) {
+    $.scrollTo = $.fn.scrollTo = function(x, y, options){
+        if (!(this instanceof $)) return $.fn.scrollTo.apply($('html, body'), arguments);
+
+        options = $.extend({}, {
+            gap: {
+                x: 0,
+                y: 0
+            },
+            animation: {
+                easing: 'swing',
+                duration: 600,
+                complete: $.noop,
+                step: $.noop
+            }
+        }, options);
+
+        //This way we can scroll to one element
+        y = y || x;
+
+        return this.each(function(){
+            var elem = $(this);
+            elem.stop().animate({
+                scrollLeft: !isNaN(Number(x)) ? x : $(x).offset().left + options.gap.x,
+                scrollTop: !isNaN(Number(y)) ? y : $(y).offset().top + options.gap.y
+            }, options.animation);
+        });
+    };
+})(jQuery);
