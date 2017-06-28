44952a32ac1c91ff2f8ce7ab01b46c7c50e2fcac
chid9202 <chid9202@gmail.com>
2017-06-28 19:50:39 +0000

    deploy.php

--- bin/deploy.php
+++ bin/deploy.php
@@ -21,7 +21,7 @@
     $output = '';
     foreach($commands AS $command){
         $tmp = shell_exec($command);
-
+        
         $output .= "<span style=\"color: #6BE234;\">\$</span><span style=\"color: #729FCF;\">{$command}\n</span><br />";
         $output .= htmlentities(trim($tmp)) . "\n<br /><br />";
     }
