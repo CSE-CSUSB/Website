39fc88cdc219e3657cb1d9e28ecec52dc972f0c4
Andrew Price <anwepr24@gmail.com>
2018-01-14 17:26:40 -0800

    winter 2018 Update 15

--- bin/contact_me.php
+++ bin/contact_me.php
@@ -10,16 +10,10 @@ if(empty($_POST['name'])      ||
    return false;
    }
    
-$name = $_POST['name'];
-$email_address = $_POST['email'];
-$message = $_POST['message'];
-$phone = $_POST['phone'];
-$message = $_POST['message'];
-
-//$name = strip_tags(htmlspecialchars($_POST['name']));
-//$email_address = strip_tags(htmlspecialchars($_POST['email']));
-//$phone = strip_tags(htmlspecialchars($_POST['phone']));
-//$message = strip_tags(htmlspecialchars($_POST['message']));
+$name = strip_tags(htmlspecialchars($_POST['name']));
+$email_address = strip_tags(htmlspecialchars($_POST['email']));
+$phone = strip_tags(htmlspecialchars($_POST['phone']));
+$message = strip_tags(htmlspecialchars($_POST['message']));
    
 // Create the email and send the message
 $to = 'cse.csusb@gmail.com'; // Add your email address inbetween the '' replacing yourname@yourdomain.com - This is where the form will send a message to.
