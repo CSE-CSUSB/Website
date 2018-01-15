fe79a5a707510ea23671004e7d5fa8a149088b6e
Andrew Price <anwepr24@gmail.com>
2018-01-14 16:14:29 -0800

    Winter 2018 Update 9
    Send us a message fix 006

--- bin/contact_me.php
+++ bin/contact_me.php
@@ -21,6 +21,6 @@ $email_subject = "Website Contact Form:  $name";
 $email_body = "You have received a new message from your website contact form.\n\n"."Here are the details:\n\nName: $name\n\nEmail: $email_address\n\nPhone: $phone\n\nMessage:\n$message";
 $headers = "From: noreply@csusb.edu\n"; // This is the email address the generated message will be from. We recommend using something like noreply@yourdomain.com.
 $headers .= "Reply-To: $email_address";   
-mail('cse.csusb@gmail.com',$email_subject,$email_body,$headers);
+mail($to,$email_subject,$email_body,$headers);
 return true;         
 ?>
